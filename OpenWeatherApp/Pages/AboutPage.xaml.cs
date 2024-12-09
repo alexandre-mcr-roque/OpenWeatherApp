d4f04a6250237b8d764c2abf580d29bbf4499b83
Alexandre Roque <alexandre.mcr.roque@gmail.com>
2024-12-09 19:31:42 +0000

    Added socials

--- OpenWeatherApp/Pages/AboutPage.xaml.cs
+++ OpenWeatherApp/Pages/AboutPage.xaml.cs
@@ -13,4 +13,29 @@ public partial class AboutPage : ContentPage
         LabelVersion.Text = $"Version:  {AppSettings.Version}";
         LabelAuthor.Text = $"Author:  {AppSettings.Author}";
     }
+
+    private async void OpenLink(string url)
+    {
+        try
+        {
+            Uri uri = new Uri(url);
+            BrowserLaunchOptions options = new BrowserLaunchOptions();
+
+            await Browser.Default.OpenAsync(uri, options);
+        }
+        catch
+        {
+            // An unexpected error occurred. No browser may be installed on the device.
+        }
+    }
+
+    private void OpenLinkedIn(object sender, EventArgs e)
+    {
+        OpenLink("https://www.linkedin.com/in/alexandre-mcr-roque");
+    }
+
+    private void OpenGitHub(object sender, EventArgs e)
+    {
+        OpenLink("https://github.com/alexandre-mcr-roque");
+    }
 }
